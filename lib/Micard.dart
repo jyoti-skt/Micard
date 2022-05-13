83fc1c1aff9687e2a50312c1dc6d21efb52dca5d
jyotisharma <jyoti.sks@yahoo.com>
2022-05-13 18:56:33 +0530

    first commit

---
+++ lib/Micard.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MICard extends StatelessWidget {
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            children: [
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
